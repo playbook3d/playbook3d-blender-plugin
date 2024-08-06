c7a91c67e5188818ba4ea75a4da9ccfad0eb261d
Alex <85909851+alexcarrizosa@users.noreply.github.com>
2024-08-06 17:38:30 -0600

    Added missing values to run call

--- addon/comfy_deploy_api/network.py
+++ addon/comfy_deploy_api/network.py
@@ -66,11 +66,35 @@ class ComfyDeployClient:
         self.mask: bytes = b""
         self.depth: bytes = b""
         self.outline: bytes = b""
-        self.normal: Optional[bytes] = b""
-        self.beauty: Optional[bytes] = b""
+        self.normal: bytes = b""
+        self.beauty: bytes = b""
+        self.style_transfer: bytes = b""
+        self.relight: bytes = b""
+        self.internal_pass: str = ""
 
     async def run_workflow(
-        self, prompt: str, prompt_a: str, prompt_b: str, prompt_c: str, prompt_d: str
+        self,
+            model: int,
+            prompt: str,
+            structure_strength: int,
+            prompt_a: str,
+            prompt_b: str,
+            prompt_c: str,
+            prompt_d: str,
+            prompt_e: str,
+            prompt_f: str,
+            prompt_g: str,
+            fixed_seed: bool,
+            style_transfer_strength: int,
+            relight_color: str,
+            relight_prompt: str,
+            relight_angle: int,
+            upscale_value: int,
+            render_mode: int,
+            retexture_enabled: bool,
+            style_transfer_enabled: bool,
+            relight_enabled: bool,
+            upscale_enabled: bool,
     ) -> str:
         if self.mask and self.depth and self.outline:
             logging.info(
@@ -89,10 +113,51 @@ class ComfyDeployClient:
             }
             run_result = requests.post(os.getenv("BASE_URL") + "/upload-images", files=files)
 
+            if retexture_enabled:
+                self.run_retexture(prompt)
+
+            if style_transfer_enabled:
+                self.run_style_transfer(style_transfer_strength)
+
+            if relight_enabled:
+                self.run_relight(relight_color, relight_prompt, relight_angle)
+
+            if upscale_enabled:
+                self.run_upscale(upscale_value)
+
             if run_result is not None:
                 return run_result.json()
             else:
                 logging.error(f"Error running workflow: {run_result}")
+
+    def run_relight(self, color: str, prompt: str, angle: int):
+        # TODO: relight
+        if color and prompt and angle:
+            relight_result = requests.post(os.getenv("BASE_URL") + "/relight")
+            if relight_result is not None:
+                self.internal_pass = relight_result.json()["internal_pass"]
+
+    def run_upscale(self, value: int):
+        # TODO: upscale
+        if value:
+            upscale_result = requests.post(os.getenv("BASE_URL") + "/upscale", json={"value": value})
+            if upscale_result is not None:
+                self.internal_pass = upscale_result.json()["internal_pass"]
+
+    def run_style_transfer(self, strength: int):
+        # TODO: style transfer
+        if strength:
+            style_transfer_result = requests.post(os.getenv("BASE_URL") + "/style-transfer",
+                                                  json={"strength": strength})
+            if style_transfer_result is not None:
+                self.internal_pass = style_transfer_result.json()["internal_pass"]
+
+    def run_retexture(self, prompt: str):
+        # TODO: retexture
+        if prompt:
+            retexture_result = requests.post(os.getenv("BASE_URL") + "/retexture", json={"prompt": prompt})
+            if retexture_result is not None:
+                self.internal_pass = retexture_result.json()["internal_pass"]
 
     def save_image(self, image: bytes, pass_type: str):
         match pass_type:
