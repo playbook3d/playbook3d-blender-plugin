e10195a27787a6685518bc6b520109c0aa79650d
Alex <85909851+alexcarrizosa@users.noreply.github.com>
2024-08-06 16:26:36 -0600

    Added classes for result, added new network logic

--- addon/comfy_deploy_api/network.py
+++ addon/comfy_deploy_api/network.py
@@ -1,54 +1,76 @@
-from comfydeploy import ComfyDeploy
-from comfydeploy.models import operations
-from typing import Dict
-import os
+import json
+from typing import Sequence
 import logging
+
+import jsonpath
 import requests
+import datetime
+
+baseUrl = "https://api.playbookengine.com"
+
+
+class ComfyDeployImage:
+    url: str
+    type: str
+    filename: str
+    subfolder: str
+
+    def __init__(self, url: str, type: str, filename: str, subfolder: str):
+        self.url = url
+        self.type = type
+        self.filename = filename
+        self.subfolder = subfolder
+
+
+class OutputData:
+    images: Sequence[ComfyDeployImage]
+
+    def __init__(self, images: Sequence[ComfyDeployImage]):
+        self.images = images
+
+
+class ComfyDeployOutput:
+    id: str
+    run_id: str
+    data: OutputData
+    created_at: datetime
+    updated_at: datetime
+
+    def __init__(self, id: str, run_id: str, data: OutputData, created_at: datetime, updated_at: datetime):
+        self.id = id
+        self.run_id = run_id
+        self.data = data
+        self.created_at = created_at
+        self.updated_at = updated_at
+
+
+class ComfyDeployResultData:
+    status: str
+    run_id: str
+    outputs: Sequence[ComfyDeployOutput]
+
+    def __init__(self, status: str, run_id: str, outputs: Sequence[ComfyDeployOutput]):
+        self.status = status
+        self.run_id = run_id
+        self.outputs = outputs
+
+
+class ComfyDeployResult:
+    status: str
+    progress: int
+    data: ComfyDeployResultData
+
+    def __init__(self, status: str, progress: int, data: ComfyDeployResultData):
+        self.status = status
+        self.progress = progress
+        self.data = data
 
 
 class ComfyDeployClient:
     def __init__(self):
-        self.client = ComfyDeploy(bearer_auth=os.getenv("COMFYDEPLOY_TOKEN", ""))
-        self.mask = ""
-        self.depth = ""
-        self.outline = ""
-
-    async def get_render_result(self, run_id: str) -> Dict:
-        return self.client.run.get(run_id=run_id).outputs
-
-    async def get_render_status(self, run_id: str) -> str:
-        return self.client.run.get(run_id=run_id).status
-
-    async def get_render_progress(self, run_id: str) -> int:
-        return self.client.run.get(run_id=run_id).progress
-
-    async def upload_image(
-        self, image_data: bytes, image_size: str, mask_type: str
-    ) -> None:
-        image_type = operations.Type.APPLICATION_OCTET_STREAM
-        logging.info(f"Starting file upload for pass: {image_type}")
-        upload_url_result = self.client.files.get_upload_url(
-            type_=image_type, file_size=image_size
-        )
-
-        if upload_url_result is not None:
-            match mask_type:
-                case "mask":
-                    self.mask = upload_url_result.file_id
-                case "depth":
-                    self.depth = upload_url_result.file_id
-                case "outline":
-                    self.outline = upload_url_result.file_id
-
-            upload_request = requests.put(
-                upload_url_result.upload_url,
-                json=image_data,
-                headers={"Content-Type": operations.Type.APPLICATION_OCTET_STREAM},
-            )
-            if upload_request.status_code != 200:
-                logging.error(f"Error uploading image: {upload_request.status_code}")
-            else:
-                logging.info(f"Image uploaded: {image_type}")
+        self.mask: bytes = b""
+        self.depth: bytes = b""
+        self.outline: bytes = b""
 
     async def run_workflow(
         self, prompt: str, prompt_a: str, prompt_b: str, prompt_c: str, prompt_d: str
@@ -58,38 +80,29 @@ class ComfyDeployClient:
                 f"Starting workflow for prompt: {prompt} "
                 f"with detailed prompt {prompt_a}, {prompt_b}, {prompt_c}, {prompt_d}"
             )
-            run_result = self.client.run.create(
-                request={
-                    "deployment_id": os.getenv("CURRENT_WORKFLOW_ID", ""),
-                    "inputs": {
-                        "general_prompt": prompt,
-                        "Prompt A": prompt_a,
-                        "Prompt B": prompt_b,
-                        "Prompt C": prompt_c,
-                        "Prompt D": prompt_d,
-                        "Prompt E": prompt,
-                        "Mask": self.mask,
-                        "Depth": self.depth,
-                        "Outline": self.outline,
-                    },
-                }
-            )
+            files = {
+                "prompt": prompt,
+                "prompt_a": prompt_a,
+                "prompt_b": prompt_b,
+                "prompt_c": prompt_c,
+                "prompt_d": prompt_d,
+                "mask": self.mask,
+                "depth": self.depth,
+                "outline": self.outline,
+            }
+            run_result = requests.post(baseUrl + "/upload-images", files=files)
 
             if run_result is not None:
-                return run_result.run_id
+                return run_result.json()
             else:
                 logging.error(f"Error running workflow: {run_result}")
 
-    async def run_test_workflow(self, prompt: str) -> str:
-        if prompt:
-            logging.info(f"Starting test workflow for prompt: {prompt}")
-            run_result = self.client.run.create(
-                request={
-                    "deployment_id": os.getenv("TEST_WORKFLOW_ID", ""),
-                    "inputs": {"positive_prompt": prompt},
-                }
-            )
-            if run_result is not None:
-                return run_result.run_id
-            else:
-                logging.error(f"Error running test workflow: {run_result}")
+    def save_image(self, image: bytes, pass_type: str):
+        match pass_type:
+            case "mask":
+                self.mask = image
+            case "depth":
+                self.depth = image
+            case "outline":
+                self.outline = image
+                
