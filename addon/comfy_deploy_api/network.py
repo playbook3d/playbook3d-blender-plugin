60434a24d0ba552c4eaa736ad9645b06f92c956d
Alex <85909851+alexcarrizosa@users.noreply.github.com>
2024-08-08 02:35:31 -0600

    Added new structure

--- addon/comfy_deploy_api/network.py
+++ addon/comfy_deploy_api/network.py
@@ -1,4 +1,3 @@
-import pathlib
 from typing import Sequence
 import logging
 import os
@@ -8,35 +7,37 @@ import numpy as np
 
 
 class GeneralSettings:
-    def __init__(self, model: int, prompt: str, strength: float) -> None:
+    def __init__(self, model: int, bias: int, prompt: str, strength: int):
         self.model = model
+        self.bias = bias
         self.scene_prompt = prompt
         self.structure_strength = strength
 
 
 class MaskSettings:
-    def __init__(self, prompt: str, color: str) -> None:
+    def __init__(self, prompt: str, color: str):
         self.mask_prompt = prompt
         self.color = color
 
 
 class StyleTransferSettings:
-    def __init__(self, strength: float) -> None:
+    def __init__(self, strength: int):
         self.style_transfer_strength = strength
 
 
 class RelightSettings:
-    def __init__(self, color: str, prompt: str, angle: int) -> None:
+    def __init__(self, color: str, prompt: str, angle: int):
         self.relight_color = color
         self.relight_prompt = prompt
         self.relight_angle = angle
 
 
 class UpscaleSettings:
-    def __init__(self, model: int, value: int, creativity: int) -> None:
+    def __init__(self, model: int, value: int, creativity: int, prompt: str):
         self.upscale_model = model
         self.upscale_value = value
         self.upscale_creativity = creativity
+        self.upscale_prompt = prompt
 
 
 class ComfyDeployImage:
@@ -118,6 +119,8 @@ class ComfyDeployClient:
         self.relight: bytes = b""
         self.internal_pass: str = ""
 
+        self.url = os.getenv("BASE_URL")
+
     async def run_workflow(
         self,
         general_settings: GeneralSettings,
@@ -128,7 +131,6 @@ class ComfyDeployClient:
         mask_settings5: MaskSettings,
         mask_settings6: MaskSettings,
         mask_settings7: MaskSettings,
-        fixed_seed: bool,
         style_transfer_settings: StyleTransferSettings,
         relight_settings: RelightSettings,
         upscale_settings: UpscaleSettings,
@@ -138,12 +140,10 @@ class ComfyDeployClient:
         relight_enabled: bool,
         upscale_enabled: bool,
     ) -> str:
-        clamped_structure_strength = np_clamp(retexture_structure_strength, 0.6, 1)
-        clamped_style_transfer_strength = np_clamp(style_transfer_strength, 0.3, 1)
-        clamped_upscale_creativity = np_clamp(upscale_settings.upscale_creativity, 0.3, 0.75)
+        clamped_structure_strength = np_clamp(general_settings.structure_strength, 0.6, 1)
 
         internal_model = ""
-        match retexture_model:
+        match general_settings.model:
             case 0:
                 internal_model = "juggernaut_reborn.safetensors"
             case 1:
@@ -156,9 +156,9 @@ class ComfyDeployClient:
                 f"Starting workflow for prompt: {general_settings.scene_prompt} "
             )
             files = {
-                "model": general_settings.model,
+                "model": internal_model,
                 "scenePrompt": general_settings.scene_prompt,
-                "structureStrength": general_settings.structure_strength,
+                "structureStrength": clamped_structure_strength,
                 "mask1": mask_settings1,
                 "mask2": mask_settings2,
                 "mask3": mask_settings3,
@@ -166,29 +166,22 @@ class ComfyDeployClient:
                 "mask5": mask_settings5,
                 "mask6": mask_settings6,
                 "mask7": mask_settings7,
-                "fixedSeed": fixed_seed,
                 "beauty": self.beauty,
                 "mask": self.mask,
                 "depth": self.depth,
                 "outline": self.outline,
             }
 
-            env_file_path = pathlib.Path(__file__).parent.parent / ".env"
-
-            # Load environment variables from the .env file
-            load_dotenv(dotenv_path=env_file_path)
-            url = os.getenv("BASE_URL")
-
-            run_result = requests.post(url + "/upload-images", data=files)
+            run_result = requests.post(self.url + "/upload-images", data=files)
 
             if retexture_enabled:
-                self.run_retexture(general_settings.scene_prompt, url)
+                self.run_retexture(general_settings.scene_prompt)
 
             if style_transfer_enabled:
-                self.run_style_transfer(style_transfer_settings, url)
+                self.run_style_transfer(style_transfer_settings)
 
             if relight_enabled:
-                self.run_relight(relight_settings, url)
+                self.run_relight(relight_settings)
 
             if upscale_enabled:
                 self.run_upscale(upscale_settings)
@@ -199,50 +192,65 @@ class ComfyDeployClient:
                 logging.error(f"Error running workflow: {run_result}")
 
     #
-    def run_retexture(self, prompt: str, url: str):
+    def run_retexture(self, prompt: str):
         # TODO: retexture
         if prompt:
             retexture_result = requests.post(
-                url + "/retexture", json={"prompt": prompt}
+                self.url + "/retexture", json={"prompt": prompt}
             )
             if retexture_result is not None:
                 self.internal_pass = retexture_result.json()["internal_pass"]
 
     #
-    def run_style_transfer(
-        self, style_transfer_settings: StyleTransferSettings, url: str
-    ):
+    def run_style_transfer(self, style_transfer_settings: StyleTransferSettings):
         # TODO: style transfer
-        strength = style_transfer_settings.style_transfer_strength
-        if strength:
-            style_transfer_result = requests.post(
-                url + "/style-transfer",
-                json={"strength": strength},
-            )
+        clamped_style_transfer_strength = np_clamp(style_transfer_settings.style_transfer_strength, 0.3, 1)
+        if clamped_style_transfer_strength:
+            files = {
+                "strength": clamped_style_transfer_strength,
+                "input_image": self.internal_pass
+            }
+            style_transfer_result = requests.post(self.url + "/style-transfer", files=files)
             if style_transfer_result is not None:
                 self.internal_pass = style_transfer_result.json()["internal_pass"]
 
     #
     def run_relight(
         self,
-        relight_settings: RelightSettings,
-        url: str,
+        relight_settings: RelightSettings
     ):
         # TODO: relight
         color = relight_settings.relight_color
         prompt = relight_settings.relight_prompt
         angle = relight_settings.relight_angle
         if color and prompt and angle:
-            relight_result = requests.post(url + "/relight")
+            files = {
+                "color": color,
+                "prompt": prompt,
+                "angle": angle,
+                "input_image": self.internal_pass
+            }
+            relight_result = requests.post(self.url + "/relight", files=files)
             if relight_result is not None:
                 self.internal_pass = relight_result.json()["internal_pass"]
 
     #
-    def run_upscale(self, upscale_settings: UpscaleSettings, url: str):
+    def run_upscale(self, upscale_settings: UpscaleSettings):
         # TODO: upscale
+        model = upscale_settings.upscale_model
         value = upscale_settings.upscale_value
-        if value:
-            upscale_result = requests.post(url + "/upscale", json={"value": value})
+        clamped_upscale_creativity = np_clamp(upscale_settings.upscale_creativity, 0.3, 0.75)
+        prompt = upscale_settings.upscale_prompt
+
+        if model and value and clamped_upscale_creativity and prompt:
+            files = {
+                "model": model,
+                "value": value,
+                "creativity": clamped_upscale_creativity,
+                "prompt": prompt,
+                "input_image": self.internal_pass
+            }
+            upscale_result = requests.post(self.url + "/upscale", files=files)
             if upscale_result is not None:
                 self.internal_pass = upscale_result.json()["internal_pass"]
 
