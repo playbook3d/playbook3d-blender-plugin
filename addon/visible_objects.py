d278843f8c6dee7ebac5cd06ee48094249c342bb
yohanjkang <yohankang0425@gmail.com>
2024-08-19 17:00:39 -0700

    Fixed mask colors

--- addon/visible_objects.py
+++ addon/visible_objects.py
@@ -6,23 +6,23 @@ original_materials: dict[str, bpy.types.Material] = {}
 
 material_props: dict[str, tuple[str, tuple]] = {
     "MASK1": ("YELLOW", (1, 0.8148, 0.0018, 1)),
-    "MASK2": ("BLUE", (0.0044, 0.2051, 0.7378, 1)),
-    "MASK3": ("TEAL", (0.1912, 0.6795, 0.7083, 1)),
-    "MASK4": ("VIOLET", (0.0006, 0, 0.0242, 1)),
-    "MASK5": ("GREEN", (0, 0.3419, 0.0844, 1)),
-    "MASK6": ("PINK", (1, 0.15, 0.6239, 1)),
-    "MASK7": ("ORANGE", (1, 0.2423, 0.0273, 1)),
-    "CATCHALL": ("RED", (0.723, 0, 0, 1)),
+    "MASK2": ("BLUE", (0.0015, 0.2501, 0.6724, 1)),
+    "MASK3": ("TEAL", (0.3614, 0.6583, 0.6653, 1)),
+    "MASK4": ("VIOLET", (0, 0, 0.0080, 1)),
+    "MASK5": ("GREEN", (0, 0.4179, 0.0976, 1)),
+    "MASK6": ("PINK", (0.8715, 0.2307, 0.6239, 1)),
+    "MASK7": ("ORANGE", (0.8551, 0.3419, 0.0482, 1)),
+    "CATCHALL": ("RED", (0.7914, 0, 0.0037, 1)),
 }
 
 color_hex: dict[str, str] = {
     "MASK1": "#ffe906",
-    "MASK2": "#0e7ddf",
-    "MASK3": "#79d7db",
-    "MASK4": "#02002b",
-    "MASK5": "#009e52",
-    "MASK6": "#ff6ccf",
-    "MASK7": "#ff872e",
+    "MASK2": "#0589d6",
+    "MASK3": "#a2d4d5",
+    "MASK4": "#000016",
+    "MASK5": "#00ad58",
+    "MASK6": "#f084cf",
+    "MASK7": "#ee9e3e",
 }
 
 
