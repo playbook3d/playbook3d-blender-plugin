3d3a1a1531e713b507b0987732dc235aac38d073
yohanjkang <yohankang0425@gmail.com>
2024-11-04 16:40:24 -0800

    New outline pass

--- Playbook/render_passes/outline_pass.py
+++ Playbook/render_passes/outline_pass.py
@@ -7,81 +7,42 @@ original_settings = {}
 
 # Save the current color management settings
 def save_outline_settings():
+    global original_settings
+
     scene = bpy.context.scene
-    view_layer = scene.view_layers["ViewLayer"]
+    view_layers = scene.view_layers["ViewLayer"]
+
+    if not scene.world:
+        scene.world = bpy.data.worlds["World"]
 
     original_settings.clear()
     original_settings.update(
         {
-            "background_color": bpy.data.worlds["World"]
-            .node_tree.nodes["RGB"]
-            .outputs[0]
-            .default_value,
-            "freestyle": scene.render.use_freestyle,
-            "freestyle_pass": view_layer.use_freestyle,
-            "render_pass": view_layer.freestyle_settings.as_render_pass,
-            "freestyle_color": bpy.data.linestyles["LineStyle"].color,
+            "z_pass": view_layers.use_pass_z,
+            "normal_pass": view_layers.use_pass_normal,
+            "position_pass": view_layers.use_pass_position,
         }
     )
-    if view_layer.freestyle_settings.linesets.active:
-        original_settings.update(
-            {
-                "silhouette": view_layer.freestyle_settings.linesets.active.select_silhouette,
-                "crease": view_layer.freestyle_settings.linesets.active.select_crease,
-                "border": view_layer.freestyle_settings.linesets.active.select_border,
-            }
-        )
 
 
 #
 def set_outline_settings():
-    scene = bpy.context.scene
-    view_layer = scene.view_layers["ViewLayer"]
+    view_layer = bpy.context.scene.view_layers["ViewLayer"]
 
-    bpy.data.worlds["World"].node_tree.nodes["Background"].inputs[0].default_value = (
-        0,
-        0,
-        0,
-        1,
-    )
-    scene.render.use_freestyle = True
-    view_layer.use_freestyle = True
-    view_layer.freestyle_settings.as_render_pass = True
-
-    if not view_layer.freestyle_settings.linesets.active:
-        view_layer.freestyle_settings.linesets.new("LineSet")
-
-    view_layer.freestyle_settings.linesets.active.select_silhouette = True
-    view_layer.freestyle_settings.linesets.active.select_crease = True
-    view_layer.freestyle_settings.linesets.active.select_border = True
-    bpy.data.linestyles["LineStyle"].color = (0, 0, 0)
+    view_layer.use_pass_z = True
+    view_layer.use_pass_normal = True
+    view_layer.use_pass_position = True
 
 
 #
 def reset_outline_settings():
-    scene = bpy.context.scene
-    view_layer = scene.view_layers["ViewLayer"]
+    global original_settings
 
-    bpy.data.worlds["World"].node_tree.nodes["Background"].inputs[0].default_value = (
-        original_settings["background_color"]
-    )
-    scene.render.use_freestyle = original_settings["freestyle"]
-    view_layer.use_freestyle = original_settings["freestyle_pass"]
-    view_layer.freestyle_settings.as_render_pass = original_settings["render_pass"]
+    view_layer = bpy.context.scene.view_layers["ViewLayer"]
 
-    if "silhouette" in original_settings:
-        view_layer.freestyle_settings.linesets.active.select_crease = original_settings[
-            "silhouette"
-        ]
-    if "crease" in original_settings:
-        view_layer.freestyle_settings.linesets.active.select_crease = original_settings[
-            "crease"
-        ]
-    if "border" in original_settings:
-        view_layer.freestyle_settings.linesets.active.select_crease = original_settings[
-            "border"
-        ]
-    bpy.data.linestyles["LineStyle"].color = original_settings["freestyle_color"]
+    view_layer.use_pass_z = original_settings["z_pass"]
+    view_layer.use_pass_normal = original_settings["normal_pass"]
+    view_layer.use_pass_position = original_settings["position_pass"]
 
 
 #
@@ -112,14 +73,42 @@ def create_outline_compositing():
 
     # Create necessary nodes
     render_layers_node = nodes.new(type="CompositorNodeRLayers")
-    alpha_node = nodes.new(type="CompositorNodeAlphaOver")
-    invert_node = nodes.new(type="CompositorNodeInvert")
+    normalize_node = nodes.new(type="CompositorNodeNormalize")
+
+    sobel_node1 = nodes.new(type="CompositorNodeFilter")
+    sobel_node1.filter_type = "SOBEL"
+    sobel_node2 = nodes.new(type="CompositorNodeFilter")
+    sobel_node2.filter_type = "SOBEL"
+    sobel_node3 = nodes.new(type="CompositorNodeFilter")
+    sobel_node3.filter_type = "SOBEL"
+
+    add_node1 = nodes.new(type="CompositorNodeMixRGB")
+    add_node1.blend_type = "ADD"
+    add_node2 = nodes.new(type="CompositorNodeMixRGB")
+    add_node2.blend_type = "ADD"
+
+    color_ramp_node = nodes.new(type="CompositorNodeValToRGB")
+    color_ramp_node.color_ramp.elements[0].position = 0.999
+
     output_node = nodes.new(type="CompositorNodeComposite")
 
     # Connect nodes
-    links.new(render_layers_node.outputs["Freestyle"], alpha_node.inputs[2])
-    links.new(alpha_node.outputs[0], invert_node.inputs[1])
-    links.new(invert_node.outputs[0], output_node.inputs["Image"])
+    # Depth pass
+    links.new(render_layers_node.outputs["Depth"], normalize_node.inputs["Value"])
+    links.new(normalize_node.outputs["Value"], sobel_node1.inputs["Image"])
+    links.new(sobel_node1.outputs["Image"], add_node1.inputs[1])
+
+    # Normal pass
+    links.new(render_layers_node.outputs["Normal"], sobel_node2.inputs["Image"])
+    links.new(sobel_node2.outputs["Image"], add_node1.inputs[2])
+
+    # Position pass
+    links.new(render_layers_node.outputs["Position"], sobel_node3.inputs["Image"])
+    links.new(sobel_node3.outputs["Image"], add_node2.inputs[2])
+
+    links.new(add_node1.outputs["Image"], add_node2.inputs[1])
+    links.new(add_node2.outputs["Image"], color_ramp_node.inputs["Fac"])
+    links.new(color_ramp_node.outputs["Image"], output_node.inputs["Image"])
 
 
 #
